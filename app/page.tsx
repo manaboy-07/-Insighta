a93930c0dd25c0d075c4abe86cd58e52840e95fd
Oruebor Manasseh <97970575+manaboy-07@users.noreply.github.com>
2026-04-29 15:02:43 +0100

    Fix(login) fixed user dashboard logins

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import Image from "next/image";
-
+import { redirect } from "next/navigation";
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main>
-        <h2>Welcome to hng task 3 web portal</h2>
-      </main>
-    </div>
-  );
+  redirect("/login");
 }
