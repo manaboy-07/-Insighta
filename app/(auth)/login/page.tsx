834355a99b8228e91cbdd01e69c28bbbfc5b75e8
Oruebor Manasseh <97970575+manaboy-07@users.noreply.github.com>
2026-04-30 14:18:43 +0100

    Fix(profile): attempted to fix profile issue

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,8 +3,9 @@ import { baseURL } from "@/app/utils/baseUrl";
 
 export default function LoginPage() {
   const login = () => {
-    window.location.href = `${baseURL}/auth/github`;
+    window.location.href = `${baseURL}/auth/github?state=web`;
   };
+
   return (
     <div className="flex bg-gray-800 h-screen items-center justify-center">
       <button
